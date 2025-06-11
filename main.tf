6cfdcb5b7b39c7c0615f0cd9f290b90c5f4f5b46
bthumati750 <bthumati750@gmail.com>
2025-06-11 17:00:56 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,6 +42,10 @@ resource "azurerm_linux_virtual_machine" "vm" {
   admin_username      = var.admin_username
   network_interface_ids = [azurerm_network_interface.nic.id]
 
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.ssh_public_key_path)
+  }
 
   os_disk {
     caching              = "ReadWrite"

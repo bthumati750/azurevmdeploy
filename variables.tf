1ad70b1edd5ecb26ed0cd4c55dd7eea2755a2656
bthumati750 <bthumati750@gmail.com>
2025-06-11 15:22:56 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "resource_group_name" {
+  type        = string
+  default     = "example-rg"
+  description = "Name of the resource group"
+}
+
+variable "location" {
+  type        = string
+  default     = "East US"
+  description = "Azure region"
+}
+
+variable "name" {
+  type        = string
+  default     = "example"
+  description = "Base name for resources"
+}
+
+variable "admin_username" {
+  type        = string
+  default     = "azureuser"
+  description = "Admin username for the VM"
+}
+
+variable "ssh_public_key_path" {
+  type        = string
+  description = "Path to your public SSH key (e.g., ~/.ssh/id_rsa.pub)"
+  default     = "~/.ssh/id_rsa.pub"
+}
